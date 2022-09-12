3b187e2cf60eee31eb80a9eefdcd2223f5942ee4
Quantum-Legend <66964980+Quantum-Legend@users.noreply.github.com>
2022-09-12 11:50:27 +0530

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -11,8 +11,11 @@ d) Display mark with highest frequency
 def Average(L):
     Sum = 0
     for i in L:
-        if i != 'NA' or i != 'na' or i != 'Na' or i != 'nA':
-            Sum = Sum + i
+        #We want the loop to count and add only when i value is a number
+        if i == 'NA' or i == 'na' or i == 'Na' or i == 'nA':
+            continue #Skips when i is NA
+        else:
+            Sum = Sum + int(i) 
     Avg = Sum/len(L)
     return Avg
 
@@ -20,17 +23,21 @@ def Average(L):
 def Highest(L):
     Hi = 0
     for i in L:
-        if i != 'NA' or i != 'na' or i != 'Na' or i != 'nA':
-            if i > Hi:
-                Hi = i
-    return Hi
+        if i == 'NA' or i == 'na' or i == 'Na' or i == 'nA':
+            continue
+        else:
+            if int(i) > Hi:
+                Hi = int(i) #Re-assigns the higher value in each iteration
+    return Hi #The final Hi value will be highest in the list
 
-def Lowest(L):
+def Lowest(L): #Same logic as Highest(), just reversed
     Lo = 100
     for i in L:
-        if i != 'NA' or i != 'na' or i != 'Na' or i != 'nA':
-            if i < Lo:
-                Lo = i
+        if i == 'NA' or i == 'na' or i == 'Na' or i == 'nA':
+            continue
+        else:
+            if int(i) < Lo:
+                Lo = int(i)
     return Lo
 
 #Count of students who were absent for the test
@@ -38,30 +45,30 @@ def Absent(L):
     c = 0
     for i in L:
         if i == 'NA' or i == 'na' or i == 'Na' or i == 'nA':
-            c +=1
+            c +=1 #Counting NA values 
     return c
 
 #Display mark with highest frequency
-def Mode(L):
+def Mode(L): #Mode is the element with highest frequency in the data
     max = 0
-    res = L[0]
+    mode = L[0]
     for i in L:
         c=0
-        for j in L:
-            if i != 'NA' or i != 'na' or i != 'Na' or i != 'nA':
-                if i == j:
-                    c+=1
-        if c>max:
-            max=c
-            res=i
-    return i
+        for j in L: #To check frequency of each element
+            if i == 'NA' or i == 'na' or i == 'Na' or i == 'nA' or j == 'NA' or j == 'na' or j == 'Na' or j == 'nA':
+                continue
+            else:
+                if int(i) == int(j):
+                    c+=1 #Counts frequency of an element in the list
+        if c>max: #Same as Highest(), but with frequency of elements
+            max=c #max contains the highest frequency
+            mode=int(i) #mode is element with that (maximum) frequency
+    return mode
 
 n = int(input("Enter class strength "))
 Marks = []
 for i in range(0,n):
     L = input("Enter marks of the student (Type NA if absent):")
-    if i != 'NA' or i != 'na' or i != 'Na' or i != 'nA':
-        L = int(L)
     Marks.append(L)
 
 print ("The average score of the class is:", Average(Marks))
