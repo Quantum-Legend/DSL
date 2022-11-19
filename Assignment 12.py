46fb286aa95aec9b6e7af52a620c126d3dfaefba
Aditya Mahale <66964980+Quantum-Legend@users.noreply.github.com>
2022-11-19 13:32:07 +0530

    Assignment 12 Completed

--- Assignment 12.py
+++ Assignment 12.py
@@ -5,6 +5,39 @@ b) Write a python program to store names and mobile numbers of your friends in s
 order on names. Search your friend from list using Fibonacci search. Insert friend if not 
 present in phonebook. 
 """
+# Add friend function
+def addFriend(arr, name, number):
+    arr.append([name, number])
+    arr.sort()
+    print("Friend added successfully")
+
+# Search friend function
+def searchFriend(arr, x):
+    print("How do you want to search your friend?")
+    print("1. Binary Search (Non Recursive)")
+    print("2. Binary Search (Recursive)")
+    print("3. Fibonacci Search")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        index = binarySearch_nonRecursive(arr, x)
+    elif choice == 2:
+        index = binarySearch_Recursive(arr, x, 0, len(arr)-1)
+    elif choice == 3:
+        index = FibonacciSearch(arr, x)
+    else:
+        print("Invalid choice")
+        return
+    if index < 0:
+        print("Friend not found")
+        print("Do you want to add friend? (y/n)")
+        ch = input()
+        if ch == 'y':
+            number = input("Enter mobile number: ")
+            addFriend(arr, x, number)
+    else:
+        print("Friend found")
+        print("Name: ", arr[index][0])
+        print("Mobile number: ", arr[index][1])
 
 # Non Recursive Binary Search function
 def binarySearch_nonRecursive(arr, x):
@@ -12,11 +45,11 @@ def binarySearch_nonRecursive(arr, x):
     low = 0
     while True:
         mid = (low + hi)//2
-        if x == arr[mid]:
+        if x == arr[mid][0]:
             return mid
         elif low == hi:
             return -1
-        elif x > arr[mid]:
+        elif x > arr[mid][0]:
             low = mid + 1
         else:
             hi = mid - 1
@@ -24,69 +57,78 @@ def binarySearch_nonRecursive(arr, x):
 # Recursive Binary Search function
 def binarySearch_Recursive(arr, x, low, hi):
     mid = (low + hi)//2
-    if x == arr[mid]:
+    if x == arr[mid][0]:
         return mid
     elif low == hi:
         return -1
-    elif x > arr[mid]:
+    elif x > arr[mid][0]:
         low = mid + 1
         return binarySearch_Recursive(arr, x, low, hi)
     else:
         hi = mid - 1
         return binarySearch_Recursive(arr, x, low, hi)
 
-#def fibonacci search function
-def fibsearch(a,x):
-    n=len(a)
-    f1=0
-    f2=1
-    F=f1+f2
+#Fibonacci Search function
+def FibonacciSearch(arr, x):
+    n = len(arr)
+    fibMm2 = 0
+    fibMm1 = 1
+    fibM = fibMm2 + fibMm1 
 
-    while(F<n):
-        f1=f2
-        f2=F
-        F=f1+f2
-    offset=-1
-    while(F>1):
+    while fibM < n: 
+        fibMm2 = fibMm1
+        fibMm1 = fibM
+        fibM = fibMm2 + fibMm1
+    
+    offset = -1
 
-        i=min(offset+f1,n-1)
+    while fibM > 1:
+        i = min(fibMm2 + offset, n-1)
+        if x > arr[i][0]:
+            fibM = fibMm1
+            fibMm1 = fibMm2
+            fibMm2 = fibM - fibMm1
+            offset = i
+        elif x < arr[i][0]:
+            fibM = fibMm2
+            fibMm1 = fibMm1 - fibMm2
+            fibMm2 = fibM - fibMm1
+        else:
+            return i
 
-        if a[i] < x :
-            F = f2
-            f2=f1
-            f1=F-f2
-            offset=i
-        elif a[i] > x:
-            F=f1
-            f2=f2-f1
-            f1=F-f2
-        else :
-            return i    
-        
-    if (f2 and a[n-1] == x):
+    if fibMm1 and arr[n-1][0] == x:
         return n-1
-
-    return -1
+    else:
+        return -1
 
 #Driver code
-seq = [1,2,4,5,6,8,9,12]
-ch = 0
-while(ch != 4):
-    ch = int(input("\nEnter your choice-\n1. Binary Search Non Recursive Method\n2. Binary Search Recursive Method\n3. Fibonacci Search\n4. Exit\n>> "))
-    if ch !=4:
-        x = int(input("Which element do you want to search?\n>> "))
-        if ch == 1:
-            i = binarySearch_nonRecursive(seq, x)
-        elif ch == 2:
-            i = binarySearch_Recursive(seq, x, 0, len(seq) - 1)
-        elif ch == 3:
-            i = fibsearch(seq, x)
+phonebook = []
+choice = 0
+while(choice != 4):
+    print("What do you want to do?")
+    print("1. Add a friend")
+    print("2. Search a friend")
+    print("3. Display Phonebook")
+    print("4. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        name = input("Enter name: ")
+        number = input("Enter number: ")
+        addFriend(phonebook, name, number)
+    elif choice == 2:
+        if len(phonebook) == 0:
+            print("Phonebook is empty")
         else:
-            print("Wrong input! Please try again:")
-
-        if i >= 0:
-            print(f"The element {x} is at {i} position")
+            searchFriend(phonebook, input("Enter name: "))
+    elif choice == 3:
+        if phonebook == []:
+            print("Phonebook is empty")
         else:
-            print(f"The element {x} is not present in the list")
+            print("\tName\t\tMobile Number")
+            for i in phonebook:
+                print(i[0], "\t|\t", i[1])
+    elif choice == 4:
+        print("Exiting...\nThank you for using the phonebook program!")
     else:
-        print("Exiting...")
+        print("Wrong input! Please try again.")
+    print()
